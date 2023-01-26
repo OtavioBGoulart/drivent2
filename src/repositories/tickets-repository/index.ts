5954d8851414fb850d31ed2363fbada58c94cdeb
Otavio Goulart <otaviobgoulartt@gmail.com>
2023-01-26 17:03:24 -0300

    feat: tickets repository

---
+++ src/repositories/tickets-repository/index.ts
@@ -0,0 +1,12 @@
+import { prisma } from "@/config";
+
+async function getTicketsTypes() {
+
+    return prisma.ticketType.findMany()
+}
+
+const ticketsRepository = {
+    getTicketsTypes
+}
+
+export default ticketsRepository;
---
+++ src/routers/payments-router.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const ticketsRouter  = Router();
+
+ticketsRouter.get("/tickets/types");
+ticketsRouter.get("/tickets");
+ticketsRouter.post("tickets");
+
+export { ticketsRouter };
